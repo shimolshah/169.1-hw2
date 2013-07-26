ac039cefe0a253d368548d522156cf9a6ddc3d73
shimol shah <shimol_shah@yahoo.com>
2013-07-25 23:02:34 -0700

    HW2-2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,16 +10,48 @@ class MoviesController < ApplicationController
     id = params["sorted"]
     # logger.info "---" + id + "---\n" unless id == nil
 
-    if params["highlight"] == "title_header" 
-      @highlight_title = 'hilite' 
-      @highlight_release_date = '' 
-      @movies = Movie.find(:all, :order => "title")
-    elsif params["highlight"] == "release_date_header"
-      @highlight_title = ''
-      @highlight_release_date = 'hilite' 
-      @movies = Movie.find(:all, :order => "release_date")
+    @all_ratings = Movie.get_ratings
+    @user_selected_ratings = params["ratings"]
+    puts @user_selected_ratings
+    
+    if @user_selected_ratings != nil
+      @movies = Movie.find(:all, :order => "title", :conditions => ["rating IN (?)", @user_selected_ratings.keys])
+      flash[:user_selected_ratings] = @user_selected_ratings
     else
-      @movies = Movie.all
+      @user_selected_ratings = flash[:user_selected_ratings]
+      if @user_selected_ratings == nil
+        @user_selected_ratings = Hash.new
+        @all_ratings.each { |rating| @user_selected_ratings[rating] = 1 }
+      else
+        flash[:user_selected_ratings] = @user_selected_ratings
+      end
+      
+      puts @user_selected_ratings
+
+      if params["highlight"] == "title_header" 
+        @highlight_title = 'hilite' 
+        @highlight_release_date = '' 
+ 
+#        if @user_selected_ratings == nil
+#          @movies = Movie.find(:all, :order => "title")
+#        else
+          @movies = Movie.find(:all, :order => "title", :conditions => ["rating IN (?)", @user_selected_ratings.keys])
+#        end
+      elsif params["highlight"] == "release_date_header"
+        @highlight_title = ''
+        @highlight_release_date = 'hilite' 
+#        if @user_selected_ratings == nil
+#          @movies = Movie.find(:all, :order => "release_date")
+#        else
+          @movies = Movie.find(:all, :order => "release_date", :conditions => ["rating IN (?)", @user_selected_ratings.keys])
+#        end
+      else
+#        if @user_selected_ratings == nil
+#          @movies = Movie.find(:all)
+#        else
+          @movies = Movie.find(:all, :conditions => ["rating IN (?)", @user_selected_ratings.keys])
+#        end
+      end
     end
   end
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,15 @@
 class Movie < ActiveRecord::Base
+  def self.get_ratings
+    #self.find(:all, :select => "rating")
+    #Movie.select("DISTINCT rating")
+        
+    result = Array.new
+    #Movie.select("DISTINCT rating").each do |movie| result << movie.rating end
+    Movie.select("DISTINCT rating").each do |movie| result << movie.rating end
+
+    #Movie.select("DISTINCT rating")
+    
+    result.sort
+    #Movie.distinct.pluck("rating")
+  end
 end
